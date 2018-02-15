f1c024a821f3b491a3ece37d691cef49fb463256
YingLi Lu <yinglilu@gmail.com>
2018-02-15 16:15:04 -0500

    fixed dos-end-of line. check_and_remote_retrieve.py: better modular.

--- check_and_remote_retrieve.py
+++ check_and_remote_retrieve.py
@@ -2,8 +2,11 @@
 '''
 script to check today's new-scan completeness on CFMM PACS and triger the retriving/converting/processing(on graham)
 
-This script will be run as a cron job:
-*/5 * * * * /usr/bin/python /usr/local/bin/retrieve_cfmm_from_sharcnet_cloud.py yinglilu '~/.ssh/id_rsa_graham.sharcnet.ca' 2>&1 1>/dev/null | /usr/bin/logger -t "retrieve_cfmm"
+This script will be run by a cron job:
+*/5 8-22 * * 1-5 /home/akhan/retrieve_cfmm/cron_cmd
+
+cron_cmd:
+    /home/akhan/retrieve_cfmm/check_and_remote_retrieve.py akhanf graham.sharcnet.ca ~/.ssh/id_rsa_graham.sharcnet.ca /project/6007967/akhanf/autobids-cfmm/bin/procNewScans 2>&1 1>/dev/null | /usr/bin/logger -t "retrieve_cfmm"
 
 algorithm:
     every 5 mins:
@@ -19,9 +22,6 @@ note: findscu is dcm4che's! not dcmtk's!!
       need docker image: yinglilu/dcm4che
       (docker pull yinglilu/dcm4che:0.1)
 
-Author: YingLi Lu
-Email: yinglilu@gmail.com
-Date: 2018-01-03
 python:2.7.13
 '''
 
@@ -47,16 +47,15 @@ def get_today_date():
     now = datetime.datetime.now()
     return now.strftime("%Y%m%d")
 
-def get_NumberOfStudyRelatedInstances(CONNECT,matching_key,username,password):
+def get_NumberOfStudyRelatedInstances(username,password,matching_key):
     '''
     find StudyInstanceUID[s] by matching key
 
     input:
-        connnect: PACS server info, for instance, 'CFMM-Public@dicom.cfmm.robarts.ca:11112'
-        matching_key: -m StudyDescription='Khan*' -m StudyDate='20171116'
         username: UWO's username to access CFMM's PACS
         password: UWO's password to access CFMM's PACS
-
+        matching_key: -m StudyDescription='Khan*' -m StudyDate='20171116'
+        
     output:string
         StudyInstanceUID1\n
         StudyInstanceUID2\n
@@ -72,22 +71,45 @@ def get_NumberOfStudyRelatedInstances(CONNECT,matching_key,username,password):
           ' {}'.format(matching_key) +\
           ' -r 00201208'+\
           ' |grep -i NumberOfStudyRelatedInstances |cut -d[ -f 2|cut -d] -f 1 |sed "/^$/d"'
-#    print(cmd) 
-    instances_str = subprocess.check_output(cmd,stderr=FNULL, shell=True)
+    try:
+        instances_str = subprocess.check_output(cmd,stderr=FNULL, shell=True)
+    except subprocess.CalledProcessError as e:
+        print 'findscu returned non-zero exit status'
+
     return instances_str
 
-def main(ssh_username,ssh_key_file,study_date):
+def have_new_scan_and_ready_for_retrieve(uwo_username,uwo_password,study_date):
     '''
-    check today's new-scan completeness on CFMM PACS and triger the retriving/converting/processing(on graham)
-
     pre=get PI+Today's NumberOfStudyRelatedInstances
-    is pre not empty:
+    if pre not empty:
         wait 20-30 seconds
         current = get PI+Today's NumberOfStudyRelatedInstances
         if pre==current (means transfer from scanner to pacs finished!)
-            procScanNew
+            return True
     '''
-        
+
+    #get NumberOfStudyRelatedInstances
+    matching_key= "-m StudyDescription='{}' -m StudyDate='{}'".format(PI_MATCHING_KEY,study_date)
+    pre = get_NumberOfStudyRelatedInstances(uwo_username,uwo_password,matching_key)
+    if pre: #if not empty, means found today's study on PACS
+        time.sleep(SLEEP_SEC)
+        current = get_NumberOfStudyRelatedInstances(uwo_username,uwo_password,matching_key)
+        if pre == current: #transfer from scanner to pacs finished, ready for retrieve
+            return True
+        else:
+            return False
+    else:
+        sys.stdout.writelines('no new data to retrieve yet!\n')
+        sys.stdout.flush()
+        return False
+
+def main(ssh_key_file,ssh_username,ssh_server,ssh_script,study_date):
+    
+    '''
+    check today's new-scan completeness(all dicoms has been sent to dicom server from scanner) on CFMM PACS and
+    triger the retriving/converting/processing(on graham)
+
+    '''
     #read uwo username and password(needed to login cfmm dicom server)
     if not os.path.exists(UWO_CREDNTIALS):
         print "need file: {}".format(UWO_CREDNTIALS)
@@ -100,47 +122,39 @@ def main(ssh_username,ssh_key_file,study_date):
     uwo_username=lines[0]
     uwo_password=lines[1]
 
-    #get NumberOfStudyRelatedInstances
-    pre = get_NumberOfStudyRelatedInstances(CONNECT,matching_key,uwo_username,uwo_password)
-
-    if pre: #if not empty, means found today's study on PACS
-        time.sleep(SLEEP_SEC)
-        current = get_NumberOfStudyRelatedInstances(CONNECT,matching_key,uwo_username,uwo_password)
-        if pre == current: #transfer from scanner to pacs finished, ready for retrieve
-            #procNewScan    
-            #ssh -i ~akhan/.ssh/id_rsa_graham.sharcnet.ca akhanf@graham.sharcnet.ca /project/6007967/akhanf/cfmm-bids/src/bin/procNewScans `date +'%Y%m%d'`'
-            cmd="ssh -i {} {}@{} {} {}".format(ssh_key_file,ssh_username,ssh_server,ssh_script,study_date)
-#	    print(cmd)
-            subprocess.check_output(cmd,shell=True)
-        else:
-            pass #do nothing, wait for the next cron task    
-    else:
-        sys.stdout.writelines('no new data to retrieve!\n')
-        sys.stdout.flush()
-
+    #triger the retriving/converting/processing(on graham) if has todday's new scan and ready for retrieve
+    if have_new_scan_and_ready_for_retrieve(uwo_username,uwo_password,study_date):
+        cmd="ssh -i {} {}@{} {} {}".format(ssh_key_file,ssh_username,ssh_server,ssh_script,study_date)
+        try:
+            stdout_stderr = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+            print stdout_stderr
+        except subprocess.CalledProcessError as e:
+            print 'findscu returned non-zero exit status'
+            
+    
 if __name__=="__main__":
 
     if len(sys.argv) == 5:
         ssh_username=sys.argv[1]
         ssh_server=sys.argv[2]
-	ssh_key_file=sys.argv[3]
+        ssh_key_file=sys.argv[3]
         ssh_script=sys.argv[4]
         study_date=get_today_date()
     elif len(sys.argv) == 6:
         ssh_username=sys.argv[1]
         ssh_server=sys.argv[2]
-	ssh_key_file=sys.argv[3]
+        ssh_key_file=sys.argv[3]
         ssh_script=sys.argv[4]
         study_date=sys.argv[5]
     else:
-        print ("Usage: python " + os.path.basename(__file__)+ " [remote user] [remote server] [ssh key] [remote script path] [date (optional, default: today)]")
-        print ("Example: python " + os.path.basename(__file__))
-        print ("         python " + os.path.basename(__file__)+ " yinglilu")
-        print ("         python " + os.path.basename(__file__)+ " yinglilu '/home/ylu/.ssh/id_rsa_graham.sharcnet.ca'")
-        print ("         python " + os.path.basename(__file__)+ " yinglilu '/home/ylu/.ssh/id_rsa_graham.sharcnet.ca' 20171116")
+        print ("Usage: python " + os.path.basename(__file__)+ " <remote user> <remote server> <ssh key> <remote script path> [date (optional, default: today)]")
+        print ("Example: python check_and_remote_retrieve.py yinglilu graham.sharcnet.ca ~/.ssh/id_rsa_graham.sharcnet.ca /project/6007967/yinglilu/autobids/bin/procNewScans 20171116")
         sys.exit(1)
 
-    print ssh_username,ssh_key_file,study_date
+    
+    main(ssh_key_file,ssh_username,ssh_server,ssh_script,study_date)
+ 
+    ##info for test
 
     # #yingli's
     # ssh_username='yinglilu'
@@ -149,15 +163,17 @@ if __name__=="__main__":
     # #ali's
     # ssh_username='akhanf'
     # ssh_key_file="~akhan/id_rsa_graham.sharcnet.ca"
-    
-    matching_key= "-m StudyDescription='{}' -m StudyDate='{}'".format(PI_MATCHING_KEY,study_date)
-    main(ssh_username,ssh_key_file,study_date)
-
+      
     #-----test code
     #test proNewScans
     #ssh -i ~/.ssh/id_rsa_graham.sharcnet.ca yinglilu@graham.sharcnet.ca /project/6007967/yinglilu/autobids/bin/procNewScans 20171116
 
-    #test script
-    #python retrieve_cfmm_from_sharcnet_cloud.py yinglilu '~/.ssh/id_rsa_graham.sharcnet.ca' 20171116
+    ##test script
+
+    #20171116 's scan
+    #python check_and_remote_retrieve.py yinglilu graham.sharcnet.ca ~/.ssh/id_rsa_graham.sharcnet.ca /project/6007967/yinglilu/autobids/bin/procNewScans 20171116
+
+    #today's new scan
+    #python check_and_remote_retrieve.py yinglilu graham.sharcnet.ca ~/.ssh/id_rsa_graham.sharcnet.ca /project/6007967/yinglilu/autobids/bin/procNewScans
 
 

1b09dfd536d41482c839e7a5ee5ab6168f40c2df
Ali Khan <alik@robarts.ca>
2021-03-01 13:17:45 -0500

    updated paths

---
+++ runNetworkJobsGraham.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+/usr/bin/ssh -i ~akhan/.ssh/id_rsa_graham.sharcnet.ca akhanf@graham.sharcnet.ca /project/ctb-akhanf/akhanf/autobids-cfmm/etc/runNetworkJobs
+
+
---
+++ runNetworkJobsGraham_sw.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+/usr/bin/ssh -i ~akhan/.ssh/id_rsa_graham_switt4.sharcnet.ca switt4@graham.sharcnet.ca /project/ctb-akhanf/switt4/autobids-pub/etc/runNetworkJobs
+
+
